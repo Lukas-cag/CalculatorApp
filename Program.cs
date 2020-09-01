e1ba3a5d2c3d2cf711af4084270e264cf4629630
Lukas Caganoff <63703252+Lukas-cag@users.noreply.github.com>
2020-09-01 15:00:09 +1000

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -15,45 +15,48 @@ namespace CalculatorApp
 
             string operation;
 
-            Console.WriteLine("Welcome to Calculator app\nPlease enter the first number\n>>> ");
+
+
+            while (true)
+            {
+            Console.Write("Welcome to Calculator app\nPlease enter the first number\n>>> ");
             num1 = Console.ReadLine();
 
-            Console.WriteLine("Please enter the second number\n>>> ");
+            Console.Write("Please enter the second number\n>>> ");
             num2 = Console.ReadLine();
 
-            Console.WriteLine("Finally, what operation would you like performed?\n>>> ");
+            Console.Write("Finally, what operation would you like performed?\n>>> ");
             operation = Console.ReadLine();
 
             int num1int = Int32.Parse(num1);
             int num2int = Int32.Parse(num1);
 
-            while (true)
-            {
+
                 if (operation.ToLower() == "add" || operation.ToLower() == "addition")
                 {
                     Console.WriteLine(num1int + num2int);
-                    break;
+                    
 
                 }
                 else if (operation.ToLower() == "subtract" || operation.ToLower() == "subtraction")
                 {
                     Console.WriteLine(num1int - num2int);
-                    break;
+                    
                 }
                 else if (operation.ToLower() == "multiply" || operation.ToLower() == "multiplication")
                 {
                     Console.WriteLine(num1int * num2int);
-                    break;
+                    
                 }
                 else if (operation.ToLower() == "divide" || operation.ToLower() == "division")
                 {
                     Console.WriteLine(num1int / num2int);
-                    break;
+                    
                 }
                 else
                 {
                     Console.WriteLine("Incorrect command try:\ndivide\nmultiply\nsubtract\nadd");
-                    break;
+                    
                 }
             }
 
